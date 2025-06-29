087172131ddcc50eb1cb096cadd5cefa9cb9d646
hiteshdusane <78253683+hiteshdusane@users.noreply.github.com>
2025-06-29 15:00:46 +0530

    feature-branch (#2)

---
+++ src/main/java/in/vhglobal/ecommerce/config/NoSecurityConfig.java
@@ -0,0 +1,19 @@
+package in.vhglobal.ecommerce.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class NoSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain noSecurity(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+                .csrf(AbstractHttpConfigurer::disable);
+        return http.build();
+    }
+}
